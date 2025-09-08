7ba4690967a3b6be651188dc5d2bb285b0c2b277
Thales Marcelino <thales.marcelino@sis-it.com>
2025-09-07 22:02:44 -0300

    feat: adiciona formatação de timestamp e função auxiliar para salvar CSV

---
+++ utils.py
@@ -0,0 +1,16 @@
+from logger import logger
+from datetime import datetime
+import pandas as pd
+
+
+def salvar_csv(data, filename):
+    try:
+        df = pd.DataFrame(data)
+        df.to_csv(filename, index=False, encoding="utf-8")
+        logger.info("INFO", f"Dados salvos em {filename}")
+    except Exception as e:
+        logger.error("ERRO", f"Erro ao salvar CSV: {e}")
+
+
+def current_timestamp():
+    return datetime.now().strftime("%d-%m-%Y %H:%M:%S")
